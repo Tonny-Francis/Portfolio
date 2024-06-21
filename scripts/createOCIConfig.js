9a54234114ea7cfe6f96f9c443779d437f62a203
Tonny Francis <tonnyfrancis161@poli.ufrj.br>
2024-06-21 19:53:06 -0300

    alter order

--- scripts/createOCIConfig.js
+++ scripts/createOCIConfig.js
@@ -4,15 +4,30 @@ const { exec } = require('child_process');
 
 let args = process.argv.slice(2);
 
+function createDirectoryAndMoveFile() {
+    exec('mkdir -p /home/runner/.oci && mv oci_api_key.pem /home/runner/.oci/oci_api_key.pem && ls /home/runner/.oci', (error, stdout, stderr) => {
+        if (error) {
+            console.error('Error creating directory');
+            return;
+        }
+
+        console.log('Directory created');
+    });
+}
+
 function downloadFile(command) {
     exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error('Error downloading file');
+            return;
         }
 
         console.log('File downloaded');
+
+        createDirectoryAndMoveFile();
     });
 }
+
 
 exec('which wget', (error, stdout, stderr) => {
     if (stdout) {
@@ -21,28 +36,5 @@ exec('which wget', (error, stdout, stderr) => {
     } else {
         downloadFile(`curl -L 'https://drive.google.com/uc?export=download&id=${args[0]}' -o oci_api_key.pem`);
         return;
-    }
-});
-
-exec('mkdir -p /home/runner/.oci', (error, stdout, stderr) => {
-    if (error) {
-        console.error('Error creating directory');
-    }
-
-    console.log('Directory created');
-});
-
-exec('mv oci_api_key.pem /home/runner/.oci/oci_api_key.pem', (error, stdout, stderr) => {
-    if (error) {
-        console.error(error)
-        console.error('Error moving file');
-    }
-
-    console.log('File moved');
-});
-
-exec('ls /home/runner/.oci', (error, stdout, stderr) => {
-    if (stdout) {
-        console.log(stdout)
     }
 });
