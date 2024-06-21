65687c0489c35d48ab33d9596c89c571178abd9a
Tonny Francis <tonnyfrancis161@poli.ufrj.br>
2024-06-21 19:40:21 -0300

    att script

--- scripts/createOCIConfig.js
+++ scripts/createOCIConfig.js
@@ -2,30 +2,46 @@ const https = require('https');
 const fs = require('fs');
 const { exec } = require('child_process');
 
-// let args = process.argv.slice(2);
+let args = process.argv.slice(2);
 
-// function downloadFile(command) {
-//     exec(command, (error, stdout, stderr) => {
-//         if (error) {
-//             console.error('Error downloading file');
-//         }
+function downloadFile(command) {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error('Error downloading file');
+        }
 
-//         console.log('File downloaded');
-//     });
-// }
+        console.log('File downloaded');
+    });
+}
 
-// exec('which wget', (error, stdout, stderr) => {
-//     if (stdout) {
-//         downloadFile(`wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=${args[0]}' -O oci_api_key.pem`);
-//         return;
-//     } else {
-//         downloadFile(`curl -L 'https://drive.google.com/uc?export=download&id=${args[0]}' -o oci_api_key.pem`);
-//         return;
-//     }
-// });
+exec('which wget', (error, stdout, stderr) => {
+    if (stdout) {
+        downloadFile(`wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=${args[0]}' -O oci_api_key.pem`);
+        return;
+    } else {
+        downloadFile(`curl -L 'https://drive.google.com/uc?export=download&id=${args[0]}' -o oci_api_key.pem`);
+        return;
+    }
+});
 
-exec('cd /home && ls', (error, stdout, stderr) => {
+exec('mkdir -p /home/runner/.oci', (error, stdout, stderr) => {
+    if (error) {
+        console.error('Error creating directory');
+    }
+
+    console.log('Directory created');
+});
+
+exec('mv oci_api_key.pem /home/runner/.oci/oci_api_key.pem', (error, stdout, stderr) => {
+    if (error) {
+        console.error('Error moving file');
+    }
+
+    console.log('File moved');
+});
+
+exec('ls /home/runner/.oci', (error, stdout, stderr) => {
     if (stdout) {
         console.log(stdout)
     }
-})
+});
